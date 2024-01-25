d2349aa2da893242133f4014e22ae48344ef7119
Jinghui Hu <hujinghui@buaa.edu.cn>
2024-01-25 11:35:41 +0800

    add sensible

--- pack/inst.sh
+++ pack/inst.sh
@@ -6,4 +6,5 @@ cd bundle/start
 git clone https://github.com/tpope/vim-pathogen.git   pathogen
 git clone https://github.com/tpope/vim-commentary.git commentary
 git clone https://github.com/tpope/vim-fugitive.git   fugitive
+git clone https://github.com/tpope/vim-sensible.git   sensible
 
