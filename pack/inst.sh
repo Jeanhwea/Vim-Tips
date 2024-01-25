553e4db5d4b4a50fddec4b2e0f7b9c3f2b5659db
Jinghui Hu <hujinghui@buaa.edu.cn>
2024-01-25 11:46:23 +0800

    add dispatch.vim

--- pack/inst.sh
+++ pack/inst.sh
@@ -5,6 +5,7 @@ mkdir -p bundle/start
 git clone https://github.com/tpope/vim-pathogen.git   ./bundle/start/pathogen
 git clone https://github.com/tpope/vim-commentary.git ./bundle/start/commentary
 git clone https://github.com/tpope/vim-fugitive.git   ./bundle/start/fugitive
+git clone https://github.com/tpope/vim-dispatch.git   ./bundle/start/dispatch
 git clone https://github.com/tpope/vim-sensible.git   ./bundle/start/sensible
 git clone https://github.com/tpope/vim-scriptease.git ./bundle/start/scriptease
 
