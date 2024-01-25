e43de97d983bc68828f055edcc2c5aaa1be829ee
Jinghui Hu <hujinghui@buaa.edu.cn>
2024-01-25 13:54:18 +0800

    add surround

--- pack/inst.sh
+++ pack/inst.sh
@@ -4,10 +4,11 @@ mkdir -p bundle/start
 
 git clone https://github.com/tpope/vim-pathogen.git   ./bundle/start/pathogen
 git clone https://github.com/tpope/vim-commentary.git ./bundle/start/commentary
-git clone https://github.com/tpope/vim-fugitive.git   ./bundle/start/fugitive
 git clone https://github.com/tpope/vim-dispatch.git   ./bundle/start/dispatch
-git clone https://github.com/tpope/vim-sensible.git   ./bundle/start/sensible
-git clone https://github.com/tpope/vim-scriptease.git ./bundle/start/scriptease
+git clone https://github.com/tpope/vim-fugitive.git   ./bundle/start/fugitive
 git clone https://github.com/tpope/vim-repeat.git     ./bundle/start/repeat
+git clone https://github.com/tpope/vim-scriptease.git ./bundle/start/scriptease
+git clone https://github.com/tpope/vim-sensible.git   ./bundle/start/sensible
+git clone https://github.com/tpope/vim-surround.git   ./bundle/start/surround
 git clone https://github.com/tpope/vim-unimpaired.git ./bundle/start/unimpaired
 
