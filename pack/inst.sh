26804104d652cef9532e896c7240858bf87da17a
Jinghui Hu <hujinghui@buaa.edu.cn>
2024-01-25 13:57:26 +0800

    update abolish.vim

--- pack/inst.sh
+++ pack/inst.sh
@@ -11,4 +11,6 @@ git clone https://github.com/tpope/vim-scriptease.git ./bundle/start/scriptease
 git clone https://github.com/tpope/vim-sensible.git   ./bundle/start/sensible
 git clone https://github.com/tpope/vim-surround.git   ./bundle/start/surround
 git clone https://github.com/tpope/vim-unimpaired.git ./bundle/start/unimpaired
+git clone https://github.com/tpope/vim-endwise.git    ./bundle/start/endwise
+git clone https://github.com/tpope/vim-abolish.git    ./bundle/start/abolish
 
