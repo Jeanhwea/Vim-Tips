9bbaa7fe28603029ae2395e379c83b78ee4e86bd
Jinghui Hu <hujinghui@buaa.edu.cn>
2024-01-25 13:50:36 +0800

    add repeat.vim unimpaired.vim

--- pack/inst.sh
+++ pack/inst.sh
@@ -8,4 +8,6 @@ git clone https://github.com/tpope/vim-fugitive.git   ./bundle/start/fugitive
 git clone https://github.com/tpope/vim-dispatch.git   ./bundle/start/dispatch
 git clone https://github.com/tpope/vim-sensible.git   ./bundle/start/sensible
 git clone https://github.com/tpope/vim-scriptease.git ./bundle/start/scriptease
+git clone https://github.com/tpope/vim-repeat.git     ./bundle/start/repeat
+git clone https://github.com/tpope/vim-unimpaired.git ./bundle/start/unimpaired
 
